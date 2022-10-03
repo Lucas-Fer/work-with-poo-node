6fe57a22bcf446ce2bf828a84a8d71a9c3b1a54b
lucas <fernandes20lucas@gmail.com>
2022-10-03 14:28:18 -0300

    feat: cria a classe Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,16 @@
+export default abstract class Race {
+  private _name: string;
+  private _desterity: number;
+  
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._desterity = dexterity;
+  }
+
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._desterity; }
+
+  static createdRacesInstances(): number { throw new Error('Not implemented'); }
+
+  abstract get maxLifePoints(): number;
+}
